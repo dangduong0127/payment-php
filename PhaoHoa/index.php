2d089834c46cf4975af1bfe9ae95dd23f46c1700
dangduong0127 <110780497+dangduong0127@users.noreply.github.com>
2023-01-03 13:52:16 +0700

    Add files via upload

---
+++ PhaoHoa/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Giàn Phun Viên</title>
+</head>
+<body>
+	<div class="single-product">
+		<div class="col">
+			<div class="thumbnail">
+			<img src="img/sg-11134201-23010-3q3re5gbvulvb4.jpg">
+		</div>
+		<div class="main">
+			<div class="title">
+				<span>Giàn phun viên</span>
+			</div>
+			<div class="price">
+				<p>550.000 VNĐ</p>
+			</div>
+			<div class="description">
+				<p>Sử dụng an toàn và hợp pháp
+chuẩn hàng bộ quốc phòng z121
+Tầm cao 25m đến 30m
+Sản phẩm nằm trong danh mục các sản phẩm được phép sử dụng của Nghị định số 137/2020/NĐ-CP của Chính phủ.</p>
+				<p>Điều 17, Nghị định 137 quy định: Cơ quan, tổ chức, doanh nghiệp, cá nhân đủ 18 tuổi trở lên có năng lực hành vi dân sự đầy đủ được sử dụng phao' hoa trong các trường hợp: lễ, tết, sinh nhật, cưới hỏi, hội nghị, khai trương, ngày kỷ niệm và trong hoạt động văn hóa, nghệ thuật.</p>
+			</div>
+			<div class="num">
+				<span>Số lượng</span>
+				<div class="count">
+					<button class="add">+</button>
+					<input type="text" name="number" value="1">
+					<button class="delete">-</button>
+				</div>
+			</div>
+			<div class="footer">
+				<button class="btn btn-success">Mua hàng</button>
+			</div>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
